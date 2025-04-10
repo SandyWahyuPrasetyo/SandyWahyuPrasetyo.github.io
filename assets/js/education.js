adbbf8ba59cf1d31ace5c5fd6825c9af7a0c1824
Sandy Wahyu Prasetyo <34877415+SandyWahyuPrasetyo@users.noreply.github.com>
2025-04-10 10:51:16 +0700

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -5,9 +5,59 @@ AOS.init();
 const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
-    title: "HTML & CSS",
+    title: "Edukasi Kesadaran Keamanan SPBE",
+    cardImage: "assets/images/education-page/dts-mono.png",
+    moocLink: "https://digitalent.komdigi.go.id/pelatihan/9968",
+  },
+  {
+    title: "Pengantar Sistem Pemerintahan Berbasis Elektronik (SPBE)",
+    cardImage: "assets/images/education-page/dts-mono.png",
+    moocLink: "https://digitalent.komdigi.go.id/pelatihan/9823",
+  },
+  {
+    title: "Sistem Manajemen Keamanan Informasi Berbasis SNI ISO/IEC 27001:2013",
+    cardImage: "assets/images/education-page/dts-mono.png",
+    moocLink: "https://digitalent.komdigi.go.id/pelatihan/5975",
+  },
+  {
+    title: "Intro to Programming",
+    cardImage: "assets/images/education-page/kaggle.png",
+    moocLink: "https://www.kaggle.com/learn/intro-to-programming",
+  },
+  {
+    title: "Responsive Web Design",
+    cardImage: "assets/images/education-page/sololearn.png",
+    moocLink: "https://www.sololearn.com/en/learn/courses/le-responsive-web-design",
+  },
+  {
+    title: "Kotlin",
+    cardImage: "assets/images/education-page/sololearn.png",
+    moocLink: "https://www.sololearn.com/en/learn/courses/le-kotlin",
+  },
+  {
+    title: "Web Development Fundamentals",
+    cardImage: "assets/images/education-page/sololearn.png",
+    moocLink: "https://www.sololearn.com/Course/web-dev-basics/?ref=app",
+  },
+  {
+    title: "Python Core",
+    cardImage: "assets/images/education-page/sololearn.png",
+    moocLink: "https://www.sololearn.com/en/learn/courses/le-python",
+  },
+  {
+    title: "C++",
+    cardImage: "assets/images/education-page/sololearn.png",
+    moocLink: "https://www.sololearn.com/en/learn/courses/c-plus-plus-introduction",
+  },
+  {
+    title: "Git",
     cardImage: "assets/images/education-page/progate.png",
-    moocLink: "https://progate.com/courses/html",
+    moocLink: "https://progate.com/courses/git",
+  },
+  {
+    title: "Command Line",
+    cardImage: "assets/images/education-page/progate.png",
+    moocLink: "https://progate.com/courses/commandline",
   },
   {
     title: "JavaScript",
@@ -15,64 +65,133 @@ const moocscards = [
     moocLink: "https://progate.com/courses/es6",
   },
   {
-    title: "Data Science",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink: "https://www.coursera.org/browse/data-science",
+    title: "HTML & CSS",
+    cardImage: "assets/images/education-page/progate.png",
+    moocLink: "https://progate.com/courses/html",
+  },
+];
+
+const certificate = document.querySelector(".certificate");
+const certificatecards = [
+  {
+    titleCertificate: "SERTIFIKAT PELATIHAN - Edukasi Kesadaran Keamanan SPBE",
+    company: "Digitalent Kominfo (Government Transformation Academy)",
+    idCertificate: "19999681320-26/GTA/BLSDM.Komdigi/2025",
+    dateIssued: "Maret 2025 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/19999681320-26.png",
+    certificateLink: "https://digitalent.komdigi.go.id/cek-sertifikat?registrasi=19999681320-26",
   },
   {
-    title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.png",
-    moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
+    titleCertificate: "SERTIFIKAT PELATIHAN - Pengantar Sistem Pemerintahan Berbasis Elektronik (SPBE)",
+    company: "Digitalent Kominfo (Micro Skill)",
+    idCertificate: "22998231320-2331/MS/BLSDM.Komdigi/2025",
+    dateIssued: "February 2025 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/22998231320.png",
+    certificateLink: "https://digitalent.komdigi.go.id/cek-sertifikat?registrasi=22998231320-2331",
   },
   {
-    title: "Machine Learning",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink: "https://www.coursera.org/learn/machine-learning",
+    titleCertificate: "SERTIFIKAT PELATIHAN - Manajemen Risiko Berbasis SNI ISO 31000",
+    company: "Badan Standardisasi Nasional",
+    idCertificate: "ELSDS/BSN/1069/XXIV",
+    dateIssued: "February 2024 - February 2025",
+    cardImageCertificate: "assets/images/certificate/ds_59508.png",
+    certificateLink: "https://elearning.bsn.go.id/sertifikat",
   },
   {
-    title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity_logo.png",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+    titleCertificate: "SERTIFIKAT PELATIHAN - SNI ISO 27001:2022 Sistem Manajemen Keamanan Informasi",
+    company: "Badan Standardisasi Nasional",
+    idCertificate: "ELSDS/BSN/634/XXIV",
+    dateIssued: "January 2024 - January 2025",
+    cardImageCertificate: "assets/images/certificate/ds_58713.png",
+    certificateLink: "https://elearning.bsn.go.id/sertifikat",
   },
   {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity_logo.png",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+    titleCertificate: "SERTIFIKAT PELATIHAN - Sistem Manajemen Keamanan Informasi Berbasis SNI ISO/IEC 27001:2013",
+    company: "Digitalent Kominfo (GTA)",
+    idCertificate: "19959751320-6/GTA/BLSDM KOMINFO/2023",
+    dateIssued: "March 2023 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/19959751320-6.png",
+    certificateLink: "https://digitalent.komdigi.go.id/cek-sertifikat?registrasi=19959751320-6",
   },
   {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity_logo.png",
-    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+    titleCertificate: "Kursus Intro to Programming",
+    company: "Kaggle",
+    idCertificate: "-",
+    dateIssued: "November 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/kaggle-introtoprogramming.png",
+    certificateLink: "https://www.kaggle.com/learn/certification/sandywahyuprasetyo/intro-to-programming",
   },
   {
-    title: "Bootstrap 4",
-    cardImage: "assets/images/education-page/udemy.png",
-    moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
+    titleCertificate: "Kursus Responsive Web Design",
+    company: "Sololearn",
+    idCertificate: "-",
+    dateIssued: "October 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/CT-YWJSESCC.jpg",
+    certificateLink: "https://www.sololearn.com/en/certificates/CT-YWJSESCC",
   },
   {
-    title: "Intro to React",
-    cardImage: "assets/images/education-page/EdX.png",
-    moocLink: "https://www.edx.org/learn/reactjs",
+    titleCertificate: "Kursus Kotlin",
+    company: "Sololearn",
+    idCertificate: "-",
+    dateIssued: "October 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/CT-BLWP9AI8.jpg",
+    certificateLink: "https://www.sololearn.com/en/certificates/CT-BLWP9AI8",
   },
   {
-    title: "Intro to React Native",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink: "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
+    titleCertificate: "Kursus Web Development Fundamentals",
+    company: "Sololearn",
+    idCertificate: "-",
+    dateIssued: "October 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/CT-ABSBKATR.jpg",
+    certificateLink: "https://www.sololearn.com/en/certificates/CT-ABSBKATR",
   },
   {
-    title: "NodeJS, Express and MongoDB",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
+    titleCertificate: "Kursus Python Core",
+    company: "Sololearn",
+    idCertificate: "-",
+    dateIssued: "September 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/CT-US2NZBL2.jpg",
+    certificateLink: "https://www.sololearn.com/en/certificates/CT-US2NZBL2",
   },
   {
-    title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.png",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
+    titleCertificate: "Kursus C++",
+    company: "Sololearn",
+    idCertificate: "-",
+    dateIssued: "September 2022 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/CT-P3IGCWR6.jpg",
+    certificateLink: "https://www.sololearn.com/en/certificates/CT-P3IGCWR6",
   },
   {
-    title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity_logo.png",
-    moocLink: "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
+    titleCertificate: "Kursus Git",
+    company: "Progate",
+    idCertificate: "-",
+    dateIssued: "April 2021 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/certificate_044d80e8qs5p1h.png",
+    certificateLink: "https://progate.com/course_certificate/044d80e8qs5p1h",
+  },
+  {
+    titleCertificate: "Kursus Command Line",
+    company: "Progate",
+    idCertificate: "-",
+    dateIssued: "April 2021 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/certificate_03a85e8bqrsrgj.png",
+    certificateLink: "https://progate.com/course_certificate/03a85e8bqrsrgj",
+  },
+  {
+    titleCertificate: "Kursus JavaScript",
+    company: "Progate",
+    idCertificate: "-",
+    dateIssued: "March 2021 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/certificate_669b9338qqu1px.png",
+    certificateLink: "https://progate.com/course_certificate/669b9338qqu1px",
+  },
+  {
+    titleCertificate: "Kursus HTML & CSS",
+    company: "Progate",
+    idCertificate: "-",
+    dateIssued: "March 2021 - No Expiration",
+    cardImageCertificate: "assets/images/certificate/certificate_a6849909qq42zz.png",
+    certificateLink: "https://progate.com/course_certificate/a6849909qq42zz",
   },
 ];
 
@@ -143,42 +262,69 @@ const showCards = () => {
 };
 document.addEventListener("DOMContentLoaded", showCards);
 
+// certificateLink
+const showCards3 = () => {
+  let output = "";
+  certificatecards.forEach(
+    ({ titleCertificate, cardImageCertificate, certificateLink, idCertificate, company, dateIssued }) =>
+    (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="300" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImageCertificate} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${certificateLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold certificate-title" style="font-size:12px;">${titleCertificate}</h6> 
+                    <h6 class="mt-0 py-2 text-center font-weight-bold certificate-title" style="font-size:12px;">${company} . ${dateIssued}</h6>  
+                    <h6 class="mt-0 py-2 text-center font-weight-bold certificate-title" style="font-size:12px;">ID: ${idCertificate}</h6>                   
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  certificate.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
 
 /* Badges*/
 
-const bagdes = document.querySelector(".badges");
-const badgesection = [
-  // {
-  //   title: "Google Developer Essentials",
-  //   image: "assets/images/education-page/badge1.png",
-  //   description: "Earned May 20, 2020",
-  // },
-  // {
-  //   title: "VM Migration",
-  //   image: "assets/images/education-page/badge2.png",
-  //   description: "Earned June 20, 2020",
-  // },
-  // {
-  //   title: "G Suite Essentials",
-  //   image: "assets/images/education-page/badge3.png",
-  //   description: "Earned July 20, 2020",
-  // },
-];
+// const bagdes = document.querySelector(".badges");
+// const badgesection = [
+//   // {
+//   //   title: "Google Developer Essentials",
+//   //   image: "assets/images/education-page/badge1.png",
+//   //   description: "Earned May 20, 2020",
+//   // },
+//   // {
+//   //   title: "VM Migration",
+//   //   image: "assets/images/education-page/badge2.png",
+//   //   description: "Earned June 20, 2020",
+//   // },
+//   // {
+//   //   title: "G Suite Essentials",
+//   //   image: "assets/images/education-page/badge3.png",
+//   //   description: "Earned July 20, 2020",
+//   // },
+// ];
 
-const showCards1 = () => {
-  let output = "";
-  badgesection.forEach(
-    ({ title, image, description }) =>
-    (output += `       
-      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
-        <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
-          <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
-          <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
-      </div>`)
-  );
-  bagdes.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards1);
+// const showCards1 = () => {
+//   let output = "";
+//   badgesection.forEach(
+//     ({ title, image, description }) =>
+//     (output += `       
+//       <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+//         <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
+//           <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
+//           <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
+//       </div>`)
+//   );
+//   bagdes.innerHTML = output;
+// };
+// document.addEventListener("DOMContentLoaded", showCards1);
 
 
 /* Timeline Section*/
